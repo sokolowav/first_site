5d2a456302d357247e659be94049407160171580
frontend <avs@webactives.ru>
2020-05-29 21:50:18 +0300

    accordion completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+  /* Клик по бургеру */
   const burger = document.querySelector('.humburger')
   burger.addEventListener('click', e => {
     const isActive = e.currentTarget.classList.value.indexOf('active') === -1 ? false : true
@@ -8,5 +10,24 @@ document.addEventListener('DOMContentLoaded', () => {
       burger.classList.remove('active')
     }
   })
+  
+  /* Клик по аккордиону */
+  const headers = [...document.querySelectorAll('.accordion-item__header')]
 
+  headers.map(i => {
+    i.addEventListener('click', function(e) {
+      const item = this.closest('.accordion-item')
+      const hasActive = item.classList.contains('active')
+      const allItems = [...this.closest('.accordion-items').querySelectorAll('.accordion-item')]
+      allItems.map(i => {
+        i.classList.remove('active')
+      })
+
+      if (hasActive) {
+        item.classList.remove('active')
+      } else {
+        item.classList.add('active')
+      }
+    })
+  })
 });
